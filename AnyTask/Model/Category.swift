5d43002f708439204ee74789ea39a7530e2d1beb
Anton Tolstov <anton@macbook.local>
2020-06-30 17:05:55 +0300

    Add Task and Category

---
+++ AnyTask/Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  AnyTask
+//
+//  Created by Anton Tolstov on 30.06.2020.
+//  Copyright © 2020 Anton Tolstov. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Identifiable {
+    let id = UUID()
+    
+    var name: String
+}
---
+++ AnyTask/Model/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  AnyTask
+//
+//  Created by Anton Tolstov on 30.06.2020.
+//  Copyright © 2020 Anton Tolstov. All rights reserved.
+//
+
+import Foundation
+
+struct Task: Identifiable {
+    let id = UUID()
+    
+    var title: String
+    var comment: String
+    
+    var deadline: Date?
+    var category: Category
+    
+    var completed: Bool
+    
+    var time: Time
+}
+
+extension Task {
+    struct Time {
+        var expected: UInt?
+        var spent: UInt?
+    }
+}
